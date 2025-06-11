7e013a203731e6e06d62acc3c2439af8a7e7d908
rakshithag27 <rakshithagreddy27@gmail.com>
2025-06-11 12:28:27 +0530

    initial commit

---
+++ synassignment01/src/main/java/com/synassignment/synassignment01/Synassignment01Application.java
@@ -0,0 +1,13 @@
+package com.synassignment.synassignment01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Synassignment01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Synassignment01Application.class, args);
+	}
+
+}
